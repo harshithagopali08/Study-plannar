d5c1b5c2d8c71e177764ba4c4a6d37b54ac33fa5
MaddiKushmi <maddikushmi@gmail.com>
2026-05-22 22:16:33 +0530

    added railway values

--- src/main/java/com/studyplanner/DBConnection.java
+++ src/main/java/com/studyplanner/DBConnection.java
@@ -4,10 +4,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/study_planner"; // change this
-    private static final String USER = "root"; // change this
-    private static final String PASSWORD = "123456"; // change this
+//    private static final String URL = "jdbc:mysql://localhost:3306/study_planner"; // change this
+//    private static final String USER = "root"; // change this
+//    private static final String PASSWORD = "123456"; // change this
+	private static final String URL ="jdbc:mysql://mysql.railway.internal:3306/railway?useSSL=false&allowPublicKeyRetrieval=true";
 
+			private static final String USER = "root";
+
+			private static final String PASSWORD = "MibMSVgnxMjgfRhBbEUgvogdGYFCODgo";
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver"); // Load the JDBC driver
         return DriverManager.getConnection(URL, USER, PASSWORD); // Connect to database
