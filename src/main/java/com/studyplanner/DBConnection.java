8454908ef233052c45b9d294801a7a6738a8ed47
harshithagopali08 <harshithaharshi094@gmail.com>
2026-05-23 12:57:20 +0530

    Update database connection details in DBConnection.java

--- src/main/java/com/studyplanner/DBConnection.java
+++ src/main/java/com/studyplanner/DBConnection.java
@@ -1,19 +1,29 @@
 package com.studyplanner;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-//    private static final String URL = "jdbc:mysql://localhost:3306/study_planner"; // change this
-//    private static final String USER = "root"; // change this
-//    private static final String PASSWORD = "123456"; // change this
-	private static final String URL ="jdbc:mysql://mysql.railway.internal:3306/railway?useSSL=false&allowPublicKeyRetrieval=true";
 
-			private static final String USER = "root";
+    private static final String URL =
+        "jdbc:mysql://kodama.proxy.rlwy.net:14958/railway?useSSL=false&allowPublicKeyRetrieval=true";
 
-			private static final String PASSWORD = "MibMSVgnxMjgfRhBbEUgvogdGYFCODgo";
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver"); // Load the JDBC driver
-        return DriverManager.getConnection(URL, USER, PASSWORD); // Connect to database
+    private static final String USER =
+        "root";
+
+    private static final String PASSWORD =
+        "ODxMEWoZhEAJxvIUqFDFdcapMLnlcRgr";
+
+    public static Connection getConnection()
+            throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
     }
 }
